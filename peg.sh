e1fd6377532d9be87fa9b46f8c51b5c187167c77
kkuramitsu <kimio@konohascript.org>
2014-12-22 11:44:17 +0700

    updated peg.sh

--- peg.sh
+++ peg.sh
@@ -3,4 +3,7 @@ if [ -z $NEZ ]; then
 	NEZ="nez.jar"
 fi
 
-java -jar $NEZ parse --memo:packrat --log:peg.csv -i input/empty -p p4d/ruby.p4d
+for file in p4d/* ; do
+  java -jar $NEZ parse --memo:packrat --log:peg.csv -i input/empty -p $file
+done
+
